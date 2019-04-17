c4e9af664f609898e1f075048df4347824e5c7cf
David <digital0david@gmail.com>
2019-04-16 22:17:03 -0400

    actions modified

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,12 @@ export const FETCH_START = "FETCH_START";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAILURE = "FETCH_FAILURE";
 
-export const getPeople = () => dispatch => {
+export const getPeople = () => {
+    return dispatch => {
     dispatch({ type: FETCH_START });
     axios
-        .get('https://swapi.co/api/people/')
+        .get(`https://swapi.co/api/people/`)
         .then(res => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
         .catch(err => dispatch({ type: FETCH_FAILURE, payload: err}));
+    };
 };
